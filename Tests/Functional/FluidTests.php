44adf99993ba37fb496f84cfa8e0ccd5c75a9d2b
Helmut Hummel <info@helhum.io>
2016-05-07 12:18:13 +0200

    A: \o/ Success!

--- Tests/Functional/FluidTests.php
+++ Tests/Functional/FluidTests.php
@@ -26,7 +26,7 @@ class FluidTests extends FunctionalTestCase
             [
                 '<ft:escapingInterceptorEnabled content="{test}" />',
                 ['test' => "<strong>Foo</strong>"],
-                '???',
+                '<strong>Foo</strong>',
             ],
         ];
     }
