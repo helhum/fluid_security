af396f31ee299d039025e8c2ca420d0708fc3b2c
Helmut Hummel <info@helhum.io>
2016-05-07 12:29:39 +0200

    A: Still XSS

--- Tests/Functional/FluidTests.php
+++ Tests/Functional/FluidTests.php
@@ -24,7 +24,7 @@ class FluidTests extends FunctionalTestCase
     {
         return [
             [
-                '<ft:escapingInterceptorEnabled content="{test}" />',
+                '{ft:escapingInterceptorEnabled(content: test)}',
                 ['test' => "<strong>Foo</strong>"],
                 '<strong>Foo</strong>',
             ],
